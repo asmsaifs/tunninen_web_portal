89eb138b84ac8c3506f008aeb431a0f9af751522
ripon23 <riponmailbox@gmail.com>
2016-08-09 13:31:35 +0600

    receint work image slideshow
    receint work image slideshow 20160809

--- views/cmslayouts/main-home.php
+++ views/cmslayouts/main-home.php
@@ -34,5 +34,53 @@ function carousel() {
     x[myIndex-1].style.display = "block";
     setTimeout(carousel, 5000); // Change image every 2 seconds
 }
+
+
+var myIndex2 = 0;
+carousel2();
+
+function carousel2() {
+    var i;
+    var x = document.getElementsByClassName("recentWork1");
+	var x1 = document.getElementsByClassName("recentWork2");
+	var x2 = document.getElementsByClassName("recentWork3");
+	var x3 = document.getElementsByClassName("recentWork4");
+	var x4 = document.getElementsByClassName("recentWork5");
+	var x5 = document.getElementsByClassName("recentWork6");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+	   x1[i].style.display = "none";
+	   x2[i].style.display = "none";
+	   x3[i].style.display = "none";
+	   x4[i].style.display = "none";
+	   x5[i].style.display = "none";
+    }
+	
+    myIndex2++;
+    if (myIndex2 > x.length) {myIndex2 = 1}
+    x[myIndex2-1].style.display = "block";
+	
+	if (myIndex2 > x1.length) {myIndex2 = 1}
+    x1[myIndex2-1].style.display = "block";
+	
+	if (myIndex2 > x2.length) {myIndex2 = 1}
+    x2[myIndex2-1].style.display = "block";
+	
+	if (myIndex2 > x3.length) {myIndex2 = 1}
+    x3[myIndex2-1].style.display = "block";
+	
+	if (myIndex2 > x4.length) {myIndex2 = 1}
+    x4[myIndex2-1].style.display = "block";
+	
+	if (myIndex2 > x5.length) {myIndex2 = 1}
+    x5[myIndex2-1].style.display = "block";
+    setTimeout(carousel2, 3000); // Change image every 2 seconds
+}
+
+
+
 </script>
+
+
+
 <?php JSRegister::end(); ?>
